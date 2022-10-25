b6233ea72c7411b57053b280b564a98a146953bb
nathanturczan <nathanturczan@gmail.com>
2022-10-25 16:04:35 -0700

    lutron device server for LAN

--- test2.js
+++ test2.js
@@ -6,36 +6,36 @@ const wss = new WebSocket.Server({port: 8082});
 wss.on("connection", ws => {
     console.log("Wave of Wellness Device Connected");
 
-
+    
     //This is all being run once the device connects
     //We run the command once, and then that's it
     //We want to create a message that is passed in as a parameter
     //logic here to run the telnet send command right here
-
+    
     ws.on("message", (data) => {
-
-      try {
+        console.log("received Message"); //For testing
         const cmd = data.toString();
-      } catch (error) {
-        console.log("Invalid command passed");
-      }
-
-      // const cmd = '#DEVICE,96,1,3'
-
-      client.send(cmd + '\r', (error, data) => {
-        if (error) {
-          return console.log('error sending a command:', error)
+        console.log(cmd); //For testing
+        
+        // const cmd = '#DEVICE,96,1,3'
+        
+        client.send(cmd + '\r', (error, data) => {
+            if (error) {
+                return console.log('error sending a command:', error)
         }
-
+        
         console.log('got response from the server:', data)
-      });
+        });
+        
     });
-
+    
     ws.on("close", () => {
         console.log("Client has disconnected");
     });
 
 });
+
+
 
 const ipAddress = '192.168.15.90'
 const port = 23
